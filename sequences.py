514da79a2d82ce63bc384d29d9a2c0c20a733bbf
ana_marquez <anamarquez.edu@hotmail.com>
2019-11-04 08:54:23 -0800

    list

---
+++ sequences.py
@@ -0,0 +1,16 @@
+
+my_name = 'Ana'
+print(my_name)
+print("\n")
+
+my_name = "Ana"
+print(my_name)
+print("\n")
+
+my_list = ["Ana", 5, 6.3]
+print(my_list)
+print("\n")
+
+print(f"The first element: {my_list[0]}")
+print(f"The 3er element: {my_list[2]}")
+print("\n")
