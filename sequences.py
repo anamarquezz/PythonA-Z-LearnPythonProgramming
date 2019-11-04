72e064086b9a4d1ed945a00ce8141f26e585bcba
ana_marquez <anamarquez.edu@hotmail.com>
2019-11-04 09:08:39 -0800

    tuples

--- sequences.py
+++ sequences.py
@@ -1,4 +1,8 @@
 
+print("LIST")
+print("\n")
+
+
 my_name = 'Ana'
 print(my_name)
 print("\n")
@@ -14,3 +18,23 @@ print("\n")
 print(f"The first element: {my_list[0]}")
 print(f"The 3er element: {my_list[2]}")
 print("\n")
+
+
+print("TUPLE")
+print("\n")
+
+tp = (15, "unwired", 10, "learning")
+print(tp)
+print(f"The first element on tuple: {tp[0]}")
+
+print("TUPLE")
+print("\n")
+
+print("Range..")
+r = range(1, 10, 2)
+print(r)
+q = list(r)
+print(q)
+
+r = list(range(1, 10))
+print(r)
