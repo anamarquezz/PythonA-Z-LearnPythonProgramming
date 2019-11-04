d128e625f5a8a01eb7a58453f5234f3878971a58
ana_marquez <anamarquez.edu@hotmail.com>
2019-11-04 10:40:57 -0800

    arithmetic, Assignment operators

---
+++ Operators.py
@@ -0,0 +1,67 @@
+
+print("\n **** ARITMETHIC ****")
+print("+ ................. Addition              4 + 7 -> 11  ")
+print("- ................. Subtraction           12 - 5 -> 7 ")
+print("* ................. Multiplication        6 * 6 -> 36")
+print("/ ................. Division              6 * 6 -> 6 ")
+print("% ................. Modulus               10 % 4 -> 2")
+print("// ................. Quotient             18 // 5 -> 3")
+print("** ................. Exponent             3 ** 5 -> 243")
+
+print("Examples:")
+a = 10
+print("\n a = 10 ")
+b = 5
+print(" b = 5 ")
+
+print(f" ( a + b ) = {a+b}")
+print(f" ( a - b ) = {a-b}")
+print(f" ( a * b )  = {a*b}")
+print(f" ( a / b )  = {a/b}")
+print(f" ( a // b )  = {a//b}")
+a = 13
+print(f" ( a % b )  = {a%b}")
+a = 2
+print(f" ( a ** b )  = {a**b}")
+
+
+print("\n **** ASSIGNMENT ****")
+print(" =           X = 5              X = 5  ")
+print(" +=          X += 5             X = X +  5 ")
+print(" -=          X -= 5             X = X -  5 ")
+print(" *=          X *= 5             X = X *  5 ")
+print(" /=          X /= 5             X = X /  5 ")
+print(" %=          X %= 5             X = X %  5 ")
+print(" //=         X //= 5            X = X // 5 ")
+print(" **=         X **= 5            X = X**5 ")
+
+print("Examples:")
+x = 10
+print("\n x = 10 ")
+y = 20
+print(" y = 20 ")
+z = x + y
+print(f" z + y {z} ")
+
+print(f" z+ x = {z+x}")
+z += x
+print(f" z+=x = {z}")
+z -= x
+print(f" z = z - x  z-=x  {z}")
+z /= x
+print(f" z = z / x  z/=x  {z}")
+
+print("\n **** UNARY MINUS ****")
+print(" =           X = 5              X = 5  ")
+print(" +=          X += 5             X = X +  5 ")
+print(" -=          X -= 5             X = X -  5 ")
+print(" *=          X *= 5             X = X *  5 ")
+print(" /=          X /= 5             X = X /  5 ")
+print(" %=          X %= 5             X = X %  5 ")
+print(" //=         X //= 5            X = X // 5 ")
+print(" **=         X **= 5            X = X**5 ")
+
+
+print("Relational")
+
+print("Logical")
