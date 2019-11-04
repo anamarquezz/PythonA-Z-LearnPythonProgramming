44dc70f6475f80d820d9c4910e34ecc79910217b
ana_marquez <anamarquez.edu@hotmail.com>
2019-11-04 09:35:11 -0800

    sets and dictionary

---
+++ sets,Dictionary.py
@@ -0,0 +1,20 @@
+print("SET \n")
+s = {10, 5, 7}
+print(s)
+
+s = {10, 8, 77, 20, 6, 5}
+print(s)
+
+list = [10, 5, 6, 7]
+
+a = set(list)
+print(a)
+
+
+print("\n DICTIONARY: \n")
+d = {10: "UNWIRED", 11: "learnking"}
+
+print(d[11])
+
+di = {1: "Ana", 2: "Marquez", 3: "Lidia"}
+print(di[2])
