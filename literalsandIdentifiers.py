3adfe988133635f4e5831c7cb99927b889a89ebb
ana_marquez <anamarquez.edu@hotmail.com>
2019-11-04 09:44:20 -0800

    Literals and Identifiers

---
+++ literalsandIdentifiers.py
@@ -0,0 +1,18 @@
+print("\n Numeric")
+a = 15
+print(a)
+
+
+print("\n Float")
+b = 5.3
+print(b)
+
+
+print("\n Binary")
+c = 0b110101
+print(c)  # decimal value
+
+d = 0x5A1C
+
+#d = identifier   = symbol  0x5A1C = Literal
+print(d)
