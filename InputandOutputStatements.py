d4436ce3a686a347851a7002b6fdf62353e17bcc
ana_marquez <anamarquez.edu@hotmail.com>
2019-11-04 16:09:28 -0800

    output statemen

---
+++ InputandOutputStatements.py
@@ -0,0 +1,21 @@
+print("\n INPUT AND OUTPUT STATEMENTS ")
+print("INPUT ------------> PROCESS -----------------> GIVE OUTPUT")
+
+print("\n Hello World")
+# function argument
+print("insert data:")
+
+print("Formatting")
+x = 5
+y = 60
+z = x * y
+print(f"Multipication of {x} and {y} is {x* y}")
+print("Multipication of {0} and {1} is {2}".format(x, y, x*y))
+print("Multipication of  %s" % z)
+num = 123.4446656
+print("The value is %f" % num)
+print("The value is %2f" % num)
+
+int_num = 150
+
+print("The values are %.2f and %d " % (num, int_num))
