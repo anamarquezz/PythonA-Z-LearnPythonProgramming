c5b8164114bbf27ffdab75551042de4aa93a629c
ana_marquez <anamarquez.edu@hotmail.com>
2019-11-04 16:52:17 -0800

    if, if, else

---
+++ conditionalififelse.py
@@ -0,0 +1,23 @@
+age = 18
+light = "Red"
+
+if age == 18:
+    print("You are now Adult ")
+else:
+    print("Not ")
+
+
+if light == "Red":
+    print("Stop")
+else:
+    print("Go")
+
+
+x = 0
+
+if x > 0:
+    print("\n Positive")
+else:
+    print("Negative")
+
+print("Done")
