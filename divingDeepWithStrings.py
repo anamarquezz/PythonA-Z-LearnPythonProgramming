02595d33eeba908042fa33f6757d38e92beae29f
ana_marquez <anamarquez.edu@hotmail.com>
2019-11-05 11:29:17 -0800

    Diving Deep With 'Strings'

---
+++ divingDeepWithStrings.py
@@ -0,0 +1,20 @@
+x = "hello world, after full stop"
+z = "      hello world, after full stop     a"
+V = "HELLO WORLD,"
+print(x.capitalize())
+print(x.split())
+print(x.upper())
+print(x.lower())
+
+
+# indexing
+print(x[0])
+print(x[0:7])
+print(x)
+print(x.strip())
+print(x.lstrip())
+print(x.rstrip())
+print(V.islower())
+print(x.isupper())
+print(x.replace("h", "S"))
+print(x.split(" "))
