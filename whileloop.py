ba8421bff57bf24fa4f6af9a0d2ee46ad70f56bb
ana_marquez <anamarquez.edu@hotmail.com>
2019-11-05 10:18:42 -0800

    while

---
+++ whileloop.py
@@ -0,0 +1,17 @@
+x = 0
+print("\n")
+while x < 6:
+    print(f"Current value of x: {x}")
+    x = x + 1
+else:
+    print("Loop Completed")
+
+num = 1
+sum = 0
+
+while num != 0:
+    num = int(input("\n \n Enter Number: "))
+    sum = sum + num
+    print(f"Number: {sum}")
+else:
+    print("Loop Completed")
