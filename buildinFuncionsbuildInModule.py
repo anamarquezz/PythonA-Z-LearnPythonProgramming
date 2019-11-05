80890d1ff5f1478e1f2f6689139de9cb4e3e61b6
ana_marquez <anamarquez.edu@hotmail.com>
2019-11-04 16:43:25 -0800

    build functions and build in Module

---
+++ buildinFuncionsbuildInModule.py
@@ -0,0 +1,19 @@
+
+
+import math
+
+print("\n **** Build in Functions ****")
+
+print("Hello World")
+# print(dir(__builtins__))
+
+print("\n **** User Defined Functions ****")
+max = max(5, 10, 55, 68, 90)
+print(f"Print max", max)
+print(f"Pow(2, 10)", {pow(2, 10)})
+
+
+print("\n **** User Defined Module ****")
+aqr = math.sqrt(5)
+print(f" math.sqrt(5) = {aqr}")
+print(dir(math))
