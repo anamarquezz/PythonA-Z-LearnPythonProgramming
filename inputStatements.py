d6bff4477e7fedd5fce9ae3fcdfed1792bb476d5
ana_marquez <anamarquez.edu@hotmail.com>
2019-11-04 16:20:02 -0800

    input statement

---
+++ inputStatements.py
@@ -0,0 +1,15 @@
+
+my_name = input("What is your name?: ")
+print(my_name)
+
+age = input("What is you age: ")
+print(age)
+print(type(age))
+
+int_age = int(age)
+print(int_age)
+print(type(int_age))
+
+age2 = int(input("What is your age? :"))
+print(age2)
+print(type(age2))
