6bc59d0e2cbe98f4fa996b63f3a4c02a5d32bfc4
ana_marquez <anamarquez.edu@hotmail.com>
2019-11-11 15:33:04 -0800

    import use module

---
+++ createandImportModules.py
@@ -0,0 +1,9 @@
+#from appModule import modulemymarth as mp
+
+import appModule.modulemymarth as mp
+
+add = mp.add(10, 15)
+print(f"Add: {add}")
+
+multiply = mp.multiply(10, 15)
+print(f"Multiply: {multiply}")
