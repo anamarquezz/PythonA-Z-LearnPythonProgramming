d104fbdd68383efd147d667c6d0a659a32c960f2
ana_marquez <anamarquez.edu@hotmail.com>
2019-11-12 08:31:30 -0800

    Python Naming Convention (Classes, Variables, Functions, Methods ...)

---
+++ NamingConvention.py
@@ -0,0 +1,34 @@
+###### Python Naming Convention (Classes, Variables, Functions, Methods ...)####
+
+# -----------------------   CLASES   -------------------------------
+#   Class : names should follow the UpperCaseCamelCase convention
+#   Python's build-in classes, however are typically lower case words
+#   Exception classes should end in "Error"
+#
+# -----------------------   INSTANCE VARIABLE   ----------------------
+#
+#   Instance variable names should be all lower case
+#   Words in an instance variable name should be separated by underscore
+#   Non-public instance variables should begin with a single underscore
+#   For private variable, two underscores may begin its name
+#
+# -----------------------   FUNCTIONS   ----------------------
+#
+#   Function names should be all lower case
+#   Words in a function name should be separated by an underscore
+#
+# -----------------------   METHODS   ----------------------
+#
+#   Function names should be all lower case
+#   Words in a function name should be separated by an underscore
+#   Privatee method should begin with a double underscore
+#
+# -----------------------   METHODS ARGUMENTS   ----------------------
+#
+#   Instance methods should have their fist argument named 'self'
+#   lowercase for parameter names
+#
+# -----------------------   CONSTANT   ----------------------
+#
+#   Constant names must be fully capitalized
+#   Words in a constant name should be separated by an underscore
