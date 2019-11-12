249b5f823932d54d44a56d94a479dbf0cf24cd35
ana_marquez <anamarquez.edu@hotmail.com>
2019-11-12 10:41:47 -0800

    Abstract Classes

---
+++ AbstractClasesMethod.py
@@ -0,0 +1,31 @@
+# Don't allow user to create object of my Parent class
+# Method of Parent class should be implemented in sub-classes
+
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):  # now is an abstract class
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Square(Shape):
+    def __init__(self, side):
+        self.__side = side
+
+    def area(self):
+        return self.__side * self.__side
+
+    def perimeter(self):
+        return 4 * self.__side
+
+
+#shape_obj = Shape()
+square_obj = Square(10)
+print(square_obj.area())
