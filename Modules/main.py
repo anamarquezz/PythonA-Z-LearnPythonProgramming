e94dc8d540cc9e5a28fc340b2b7961cfed70d65a
ana_marquez <anamarquez.edu@hotmail.com>
2019-11-11 16:13:34 -0800

    Multiple Inheritance

--- Modules/main.py
+++ Modules/main.py
@@ -4,9 +4,11 @@ from triangle import Triangle as tq
 
 s1 = sq()
 s1.set_value(8, 15)
-print(s1.area())
+s1.set_color("Blue")
+print(s1.area(), s1.get_color())
 
 
 t1 = tq()
 t1.set_value(5, 10)
-print(t1.area())
+t1.set_color("Green")
+print(t1.area(), t1.get_color())
--- Modules/square.py
+++ Modules/square.py
@@ -1,7 +1,8 @@
 #import Polygon
 from Polygon import Polygon
+from shape import Shape
 
 
-class Square(Polygon):
+class Square(Polygon, Shape):
     def area(self):
         return self.get_width() * self.get_height()
