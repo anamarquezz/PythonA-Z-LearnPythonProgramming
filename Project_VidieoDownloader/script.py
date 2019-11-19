5f23952d09aa600d097beaff466f66624beb1bad
ana_marquez <anamarquez.edu@hotmail.com>
2019-11-19 14:45:53 -0800

    Video Downloader - Part 2

--- Project_VidieoDownloader/script.py
+++ Project_VidieoDownloader/script.py
@@ -2,9 +2,28 @@
 
 from pytube import YouTube
 
+'''
 yt = YouTube("https://www.youtube.com/watch?v=_1fuQco3Tes")
 print(yt.streams.all())
 print("\n")
 dw = yt.streams.get_by_itag(22)
 dw.download(
     "c:\\Users\\anamarquez\\source\\Repass\\PythonA-Z-LearnPythonProgramming\\Project_VidieoDownloader\\")
+
+
+
+'''
+# download multiple videos
+
+# video_list = ["https://www.youtube.com/watch?v=UBnfm4s7CRA",
+#             "https://www.youtube.com/watch?v=I8uqz-hQnZs", "https://www.youtube.com/watch?v=_lwn4uDGKfU"]
+video_list = open(
+    "c:\\Users\\anamarquez\\source\\Repass\\PythonA-Z-LearnPythonProgramming\\Project_VidieoDownloader\\sample.txt", "r")
+
+for vds in video_list:
+    yt = YouTube(vds)
+    dw = yt.streams.get_by_itag(22)
+    print(yt.streams.all())
+    print("\n")
+    dw.download(
+        "c:\\Users\\anamarquez\\source\\Repass\\PythonA-Z-LearnPythonProgramming\\Project_VidieoDownloader\\")
